7f5cef1c4c28742f3ab7d6a0cca00edbe4481047
Kuldeep <96681815+wwwks@users.noreply.github.com>
2022-08-18 22:27:03 +0530

    Update DirectMovies.js

--- DirectMovies.js
+++ DirectMovies.js
@@ -1,25 +1,6 @@
-$('.search-button').click(function(){
-    $(this).parent().toggleClass('open');
-  });
-
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('searchform');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
-
+document.querySelector("#header > div.header-search")
+document.querySelector("#searchform")
+document.querySelector("#searchform > fieldset")
+document.querySelector("#s")
+document.querySelector("#search-image")
 
